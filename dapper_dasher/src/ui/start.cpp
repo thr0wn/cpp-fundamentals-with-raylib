8359f49c269dd7abec3d74e880a077ee64cb9cf0
Natan Camargos <natanscamargos@gmail.com>
2025-12-02 13:48:52 -0300

    try to add raygui

--- dapper_dasher/src/ui/start.cpp
+++ dapper_dasher/src/ui/start.cpp
@@ -1,11 +1,22 @@
 #include "start.hpp"
 
-void Start::renderOut() {
+void Start::start() {
+  textNewGameRec.x = (0.25f / 10.f) * windowWidth;
+  textNewGameRec.y = ((6.5f / 10.f) * windowHeight);
+  textNewGameRec.width = .5f * START_TEXT_SIZE * textNewGame.length();   
+  textNewGameRec.height = 1.2f * START_TEXT_SIZE;   
+
+  textQuitGameRec.x = (0.25f / 10.f) * windowWidth;
+  textQuitGameRec.y = (8.0f / 10.f) * windowHeight;
+  textNewGameRec.width = .5f * START_TEXT_SIZE * textQuit.length();   
+  textNewGameRec.height = 1.2f * START_TEXT_SIZE;     
+}
+
+void Start::render() {
   if (!GameService::isStartUI()) {
     return;
   }
-  DrawText(textNewGame, (int)((0.25f / 10.f) * windowWidth),
-           (int)((6.5f / 10.f) * windowHeight), START_TEXT_SIZE, WHITE);
-  DrawText(textQuit, (int)((0.25f / 10.f) * windowWidth),
-           (int)((8.0f / 10.f) * windowHeight), START_TEXT_SIZE, WHITE);
+  GuiButton(textNewGameRec, textNewGame.data());
+//  DrawText(textNewGame.data(), textNewGameRec.x, textNewGameRec.y, START_TEXT_SIZE, WHITE);
+  // DrawText(textQuit.data(), textQuitGameRec.x, textQuitGameRec.y, START_TEXT_SIZE, WHITE);
 }
--- dapper_dasher/src/ui/start.hpp
+++ dapper_dasher/src/ui/start.hpp
@@ -3,15 +3,25 @@
 #include "game-node.hpp"
 #include "game-service.hpp"
 #include "raylib.h"
+#include <string>
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
 #define START_TEXT_NEW_GAME "New Game"
 #define START_TEXT_QUIT "Quit"
 #define START_TEXT_SIZE 48
 
 class Start : public GameNode {
-  const char *textNewGame = START_TEXT_NEW_GAME;
-  const char *textQuit = START_TEXT_QUIT;
+  const std::string textNewGame = START_TEXT_NEW_GAME;
+  Rectangle textNewGameRec;
+  bool textNewGameIsPressed = false;  
+  
+  const std::string textQuit = START_TEXT_QUIT;
+  Rectangle textQuitGameRec;
+  bool textQuitIsPressed = false;    
 
 public:
-  void renderOut() override;
+  void start() override;
+  void render() override;
 };
