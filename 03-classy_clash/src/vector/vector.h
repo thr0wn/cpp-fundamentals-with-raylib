1f4315105d36316b9167b75ee8bb364e76bada43
Natan Camargos <natanscamargos@gmail.com>
2026-01-12 19:48:21 -0300

    minor refactoring

---
+++ 03-classy_clash/src/vector/vector.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "raylib.h"
+
+namespace game {
+namespace Vector {
+void copy(Vector2 &source, Vector2 &target);
+void copy(Vector3 &source, Vector3 &target);
+} // namespace VectorUtils
+} // namespace game
