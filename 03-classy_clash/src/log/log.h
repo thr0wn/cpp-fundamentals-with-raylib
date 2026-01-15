7c466a347ff01e45b0e7e6026abc288499b6111d
Natan Camargos <natanscamargos@gmail.com>
2026-01-15 17:16:19 -0300

    remove not used emitter reference

--- 03-classy_clash/src/log/log.h
+++ 03-classy_clash/src/log/log.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "async/async-pointer.h"
-#include "event/emitter.h"
 #include <iostream>
 #include <string>
 #include <fmt/format.h>
@@ -9,7 +8,6 @@ namespace game {
 class Log {
 private:
   bool _enabled = true;
-  Emitter *_emitter = AsyncPointer::get<Emitter>();  
 public:
   void info(const std::string &message);
 
